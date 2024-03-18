c3f7e4638d7bedbd2ec48cd30f54cbeedc27180b
Miron Laura <miron_laura19911@yahoo.com>
2024-03-18 19:37:32 +0200

    new

---
+++ OnlineCourseApp/Data/Entity/BlogPostEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entity
+{
+    public class BlogPostEntity
+    {
+      
+        
+
+            public BlogPostEntity(Guid _id, string _title, string _description)
+            {
+
+                id = _id;
+                title = _title;
+                description = _description;
+
+            }
+        public Guid id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+
+    }
+
+}
---
+++ OnlineCourseApp/Servicess/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ServicesIntrface;
+
+
+namespace Servicess
+{
+    public static class DependencyInjection
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IBlogPostEntityServiceInterfaces, BlogPostService>();
+        }
+    }
+}
